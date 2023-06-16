df8ce0bec3ccc8c632f991dfed1a9da39f3d1078
rashuakbar <victormf25@outlook.pt>
2023-06-16 18:08:39 +0100

    some ko. need to treat case without new line

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: vimendes <vimendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 19:25:13 by vimendes          #+#    #+#             */
-/*   Updated: 2023/06/16 18:06:53 by vimendes         ###   ########.fr       */
+/*   Updated: 2023/06/16 18:08:20 by vimendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <fcntl.h>
 
 #ifndef BUFF_SIZE
-# define BUFF_SIZE 1git0
+# define BUFF_SIZE 10
 #endif
 
 char	*get_next_line(int fd);
